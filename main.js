8077adbdd6b42afb7850c868778847ef5eae9c2d
codemantra10 <73803525+codemantra10@users.noreply.github.com>
2021-09-02 20:39:47 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,26 +1,43 @@
+object=[]
 img=""
+insta_status="";
 function preload(){
 img=loadImage("dog_cat.jpg");
 }
 function setup(){
 canvas=createCanvas(500,500);
 canvas.position(500,200);
+obj_detector=ml5.objectDetector("cocossd",ModelLoaded);
+document.getElementById("Status").innerHTML="Status:Loading Objects"+"<img src='load.webp' style='width:50px; height:50px;'>";
 }
 function draw(){
+
 image(img,0,0,500,500);
-noFill();
-rect(120, 25, 125, 250);
-stroke(0, 247, 255);
-text("dog",90,25);
-textSize(20);
-strokeWeight(3);
-
-fill(0, 247, 255);
-textSize(20);
-text("cat",220,25);
-noFill();
-strokeWeight(3);
-stroke(255, 0, 0);
-rect(240, 25, 125, 250);
-
+if (insta_status!="") {
+for ( i = 0; i< object.length; i++) {
+    document.getElementById("Status").innerHTML="Status:Object Detected✔";
+    document.getElementById("Status").style.color="lightgreen"; 
+    fill(0, 247, 255);  
+    confidence=floor((object[i].confidence)*100)+"%";
+    textSize(20);
+    text(object[i].label+" "+confidence,object[i].x+15,object[i].y+15);
+    noFill();
+    stroke(0, 247, 255);
+    rect(object[i].x, object[i].y, object[i].width, object[i].height);
+}    
+}
 };
+function ModelLoaded(){
+console.log("I sleepy,so I write this, the model a ok!😴👍🏽");
+insta_status=true;
+obj_detector.detect(img,gotResult);
+}
+function gotResult(error,result){
+if (error) {
+console.error(error);   
+} else {
+console.log(result);
+object=result
+
+}
+}
